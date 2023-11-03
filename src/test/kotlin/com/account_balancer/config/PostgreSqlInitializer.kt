9fd96aa14231dd669c3870355e2ac9b2c52c9e77
fliur <kfliurkin@gmail.com>
2023-11-03 17:38:14 +0300

    typo fix

--- src/test/kotlin/com/account_balancer/config/PostgreSqlInitializer.kt
+++ src/test/kotlin/com/account_balancer/config/PostgreSqlInitializer.kt
@@ -6,7 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
-class PostgresqlInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+class PostgreSqlInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
     override fun initialize(applicationContext: ConfigurableApplicationContext) {
         TestPropertyValues.of(
             "spring.datasource.url=${POSTGRESQL_CONTAINER.getJdbcUrl()}",
